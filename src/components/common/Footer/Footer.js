d18d9b4160546e4eb2c24f72272542f8f9c2d551
Edgar López <edgar.lr.18@gmail.com>
2021-01-12 22:23:56 -0600

    Remove react-icons, add jsx icons, add styles to footer

--- src/components/common/Footer/Footer.js
+++ src/components/common/Footer/Footer.js
@@ -1,29 +1,66 @@
 import React from "react"
 import styled from "styled-components"
-import SocialMedia from "../SocialMedia/SocialMedia"
+import { CONTACT_EMAIL, CONTACT_TEL } from "../../../lib/contants"
+import ExternalLink from "../../ui/ExternalLink"
 
-const Div = styled.div`
-  padding: 4em 2em 1em;
+import { BEHANCE_URL, INSTAGRAM_URL } from "../../../lib/contants"
+import Behance from "../../Icons/Behance"
+import Instagram from "../../Icons/Instragram"
+
+const StyledFooter = styled.div`
+  padding: 2em 0;
   border-top: 1px solid #0a3;
-  & p {
-    font-size: 0.8em;
+  text-align: center;
+  li {
+    font-size: 1em;
     margin-top: 0.5em;
+  }
+  .links {
+    text-decoration: underline;
+    color: #000;
+    padding: 0.5rem 0;
+  }
+  .info {
+    font-size: 0.875rem;
+    padding: 0.5rem 0;
+  }
+  .social-media {
+    display: flex;
+    margin: 0 auto;
+    justify-content: center;
+    li {
+      padding: 0.5rem;
+    }
   }
 `
 
 export default function Footer() {
   return (
-    <Div>
-      <ul>
-        <li>info@thepapermatter.com</li>
+    <StyledFooter>
+      <ul className="links">
+        <li>
+          <ExternalLink to={`mailto:${CONTACT_EMAIL}`}>
+            {CONTACT_EMAIL}
+          </ExternalLink>
+        </li>
         <li>Política de privacidad</li>
-        <li>Ciudad de México, México </li>
-        <li>5511223344</li>
+      </ul>
+      <ul className="info">
+        <li>Ciudad de México, México - {CONTACT_TEL}</li>
         <li>(C) 2020 ThePaperMatter</li>
       </ul>
-      <div style={{padding: '64px 0'}}>
-        <SocialMedia />
-      </div>
-    </Div>
+      <ul className="social-media">
+        <li>
+          <ExternalLink to={INSTAGRAM_URL}>
+            <Instagram />
+          </ExternalLink>
+        </li>
+        <li>
+          <ExternalLink to={BEHANCE_URL}>
+            <Behance />
+          </ExternalLink>
+        </li>
+      </ul>
+    </StyledFooter>
   )
 }
