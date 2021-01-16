765a3e03fd0a93a9632c92c38cc1963d0bca9c64
Edgar López <edgar.lr.18@gmail.com>
2021-01-16 15:15:44 -0600

    Add portfolioImageCard, remove unused portfolio components

--- src/components/Portfolio/PortfolioContainer.js
+++ src/components/Portfolio/PortfolioContainer.js
@@ -1,54 +1,23 @@
-import React, { useState } from "react"
-import CardContainer from "./CardContainer/CardContainer"
-import { colors, breakpoints } from "../../styles/Vars"
-import withLocation from "../HoC/withLocation"
-import ButtonComponent from "../ui/ButtonComponent"
-import PortfolioNav from "./PortfolioNav"
+import React from "react"
+import { breakpoints } from "../../styles/Vars"
 import styled from "styled-components"
+import PortfolioImageCard from "./PortfolioImageCard"
 
-const PortfolioContainer = ({ categories, projects }) => {
-  const { category } = search
-  const [loading, setLoading] = useState(true)
+const StyledPortafolioContainer = styled.div`
+  padding: 1rem 0;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+`
 
-  // const filterData = async (category = "todo") => {
-  //   if (category === "todo") {
-  //     setVisibleData(data)
-  //   } else {
-  //     const filteredData = data.filter(
-  //       project => project.project_category.slug === category
-  //     )
-  //     setVisibleData(filteredData)
-  //   }
-  // }
-
-  const handleTabClick = async tab => {
-    // filterData(tab)
-  }
-
+const PortfolioContainer = ({ images }) => {
   return (
     <StyledPortafolioContainer>
-      <PortfolioNav
-        categories={navData}
-        handleTabClick={handleTabClick}
-        activeCategory={category}
-      />
-
-      <CardContainer data={visibleData} loading={loading} />
+      {images.map(image => (
+        <PortfolioImageCard image={image} key={image.id} />
+      ))}
     </StyledPortafolioContainer>
   )
 }
 
-const StyledPortafolioContainer = styled.div`
-  padding-top: 100px;
-  ${breakpoints.tablet} {
-    padding: 20vh 0 5em;
-  }
-  ${breakpoints.laptop} {
-    padding: 20vh 2.5em 5em;
-  }
-  ${breakpoints.laptopL} {
-    padding: 30vh 5em 5em;
-  }
-`
-
-export default withLocation(PortfolioContainer)
+export default PortfolioContainer
--- src/pages/portafolio.js
+++ src/pages/portafolio.js
@@ -2,7 +2,7 @@ import React from "react"
 import { Layout } from "../components/common/Layout"
 import SEO from "../components/common/seo"
 import { graphql } from "gatsby"
-// import PortfolioContainer from "../components/portfolio/PortfolioContainer"
+import PortfolioContainer from "../components/portfolio/PortfolioContainer"
 import { PageCover } from "../components/common/PageCover"
 import PortfolioNav from "../components/portfolio/PortfolioNav"
 
@@ -21,6 +21,10 @@ export const portfolioQuery = graphql`
       nodes {
         id
         title
+        client {
+          name
+          url
+        }
         image {
           childImageSharp {
             fluid(maxWidth: 300) {
@@ -45,7 +49,7 @@ export default function portafolio({ data, location }) {
 
       <PortfolioNav categories={data.allStrapiServices.nodes} />
 
-      {/* <PortfolioContainer categories={data.allStrapiServices.nodes} /> */}
+      <PortfolioContainer images={data.allStrapiImages.nodes} />
     </Layout>
   )
 }
