fb38351f603b3a36f845c46add33de08c64b6331
Edgar López <edgar.lr.18@gmail.com>
2021-01-31 21:55:40 -0600

    Fix services layout

--- src/components/sections/ServicesSection.js
+++ src/components/sections/ServicesSection.js
@@ -2,46 +2,7 @@ import React from "react"
 import styled from "styled-components"
 import { Link } from "gatsby"
 import Img from "gatsby-image"
-
-const StyledServicesSection = styled.section`
-  display: flex;
-  flex-wrap: wrap;
-  .service-card {
-    width: 350px;
-    height: 36rem;
-    border-bottom: 2px dashed var(--gray-25);
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    position: relative;
-  }
-  .service-card:last-child {
-    border-bottom: 0;
-  }
-  .info {
-    position: absolute;
-    z-index: 2;
-    a {
-      text-decoration: underline;
-    }
-    h3 {
-      margin: 1.5rem 0 0;
-      font-size: var(--font-xl);
-    }
-    p {
-      margin: 2rem 0;
-    }
-  }
-`
-
-const StyledImg = styled(Img)`
-  z-index: 1;
-  position: absolute;
-  height: 26rem;
-  width: 100%;
-  margin: 0;
-  opacity: 0.5;
-`
+import { breakpoints } from "../../styles/Vars"
 
 export default function ServicesSection({ services }) {
   return (
@@ -65,3 +26,77 @@ export default function ServicesSection({ services }) {
     </StyledServicesSection>
   )
 }
+
+const StyledServicesSection = styled.section`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  ${breakpoints.laptop} {
+    &:hover .service-card {
+      opacity: 0.1;
+    }
+  }
+  .service-card {
+    width: 50%;
+    min-width: 350px;
+    height: 36rem;
+    border-bottom: 2px dashed var(--gray-25);
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    position: relative;
+    cursor: pointer;
+    transition: opacity 0.35s;
+    ${breakpoints.laptop} {
+      border: 1px solid var(--gray-50);
+      &:first-child,
+      :nth-child(2) {
+        border-top: none;
+      }
+      &:nth-child(odd):nth-last-child(even) {
+        border-bottom: none;
+      }
+      &:hover {
+        opacity: 1;
+      }
+    }
+  }
+  .service-card:last-child {
+    border-bottom: 0;
+  }
+  .info {
+    position: absolute;
+    z-index: 2;
+    width: 100%;
+    pointer-events: none;
+    ${breakpoints.laptop} {
+      padding: 4rem;
+    }
+    a {
+      text-decoration: underline;
+    }
+    h3 {
+      margin: 1.5rem 0 0;
+      font-size: var(--font-xl);
+    }
+    p {
+      margin: 2rem 0;
+    }
+  }
+`
+
+const StyledImg = styled(Img)`
+  z-index: 1;
+  position: absolute;
+  height: 26rem;
+  width: 100%;
+  margin: 0;
+  opacity: 0.5;
+  ${breakpoints.laptop} {
+    opacity: 0;
+    transition: opacity 0.35s;
+    &:hover {
+      opacity: 0.5;
+    }
+  }
+`
