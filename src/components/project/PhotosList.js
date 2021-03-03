242c72123046d640095550746e3c385cccc96d86
Edgar López <edgar.lr.18@gmail.com>
2021-03-03 00:41:19 -0600

    Add grid to project photos

--- src/components/project/PhotosList.js
+++ src/components/project/PhotosList.js
@@ -1,13 +1,32 @@
 import React, { useState } from "react"
 import styled from "styled-components"
 import Img from "gatsby-image"
-import { responsiveSectionMargin } from "../../styles/utils"
+import { breakpoints } from "../../styles/utils"
 import Lightbox from "../UI/Lightbox"
 import { useUI } from "../UI/use-ui"
 
 const StyledPhotoList = styled.div`
   padding: 2rem 0;
-  ${responsiveSectionMargin}
+  margin: 0 -1.5rem;
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  > div {
+    flex: 0 0 100%;
+  }
+  ${breakpoints.tablet} {
+    margin: 0 -6rem;
+  }
+  ${breakpoints.laptop} {
+    margin: 0;
+    > div:nth-child(3n),
+    div:nth-child(3n -1) {
+      flex: 0 0 48%;
+      div {
+        height: 25rem;
+      }
+    }
+  }
 `
 
 const StyledImg = styled(Img)`
