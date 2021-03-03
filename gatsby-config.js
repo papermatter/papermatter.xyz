15feca4776d4fd9f3d790cef7863ff16187b1bce
Edgar López <edgar.lr.18@gmail.com>
2021-03-03 14:34:15 -0600

    Update favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -62,7 +62,7 @@ module.exports = {
         background_color: `#131414`,
         theme_color: `#131414`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/logo.svg`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
