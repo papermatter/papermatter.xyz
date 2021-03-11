d5f26ddbabd431df8ab3d7c967655b5c07093105
Edgar López <edgar.lr.18@gmail.com>
2021-03-11 13:58:05 -0600

    Add useIntersectionObserver

--- src/components/common/PageCover/PageCover.js
+++ src/components/common/PageCover/PageCover.js
@@ -1,20 +1,61 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
+import BackgroundImage from "gatsby-background-image"
 import Img from "gatsby-image"
 import styled from "styled-components"
 import PropTypes from "prop-types"
 import { breakpoints, responsiveSectionMargin } from "../../../styles/utils"
+import { useIntersectionObserver } from "../../../lib/hooks/use-intersection-observer"
 
 const StyledImg = styled(Img)`
+  width: 100%;
+  height: auto;
+`
+const StyledPageCover = styled.div`
   ${responsiveSectionMargin}
+  position: relative;
   width: 100vw;
-  height: 70vh;
+  height: 50vh;
+  overflow: hidden;
+  display: flex;
+
+  .img-container {
+    width: 100%;
+    height: auto;
+    position: absolute;
+    bottom: 0;
+  }
   ${breakpoints.laptop} {
     height: 100vh;
   }
 `
 
 const PageCover = ({ src, alt }) => {
-  return <StyledImg fluid={src} alt={alt} />
+  const [offset, setOffset] = useState(0)
+
+  const { isIntersecting, ref } = useIntersectionObserver()
+
+  useEffect(() => {
+    const onScroll = () => {
+      if (isIntersecting) {
+        setOffset(window.scrollY)
+      }
+    }
+
+    window.addEventListener("scroll", onScroll)
+    return () => window.removeEventListener("scroll", onScroll)
+  }, [isIntersecting])
+
+  return (
+    <StyledPageCover ref={ref}>
+      <div className="img-container">
+        <StyledImg
+          fluid={src}
+          alt={alt}
+          style={{ transform: `translateY(${offset * 0.3}px)` }}
+        />
+      </div>
+    </StyledPageCover>
+  )
 }
 
 export default PageCover
---
+++ src/lib/hooks/use-intersection-observer.js
@@ -0,0 +1,32 @@
+import { useEffect, useRef, useState } from "react"
+
+export const useIntersectionObserver = ({ rootMargin = "10px" } = {}) => {
+  const [isIntersecting, setIsIntersecting] = useState(false)
+
+  const ref = useRef(null)
+
+  useEffect(() => {
+    let observer
+
+    const onChange = entries => {
+      const el = entries[0]
+      if (el.isIntersecting) {
+        setIsIntersecting(true)
+      } else {
+        setIsIntersecting(false)
+      }
+    }
+
+    if (typeof IntersectionObserver !== "undefined") {
+      observer = new IntersectionObserver(onChange, {
+        rootMargin: rootMargin,
+      })
+
+      observer.observe(ref.current)
+    }
+
+    return () => observer && observer.disconnect()
+  }, [])
+
+  return { isIntersecting, ref }
+}
